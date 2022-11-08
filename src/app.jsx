feac80d0ab130cdaa4d385205b5f2a89f2fbb9d8
Frontend_boantek <fabricioreynacordes@gmail.com>
2022-11-08 17:54:04 -0300

    Reciente

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+
+import MainHeader from "./components/header/Cabeza.Html";
+import MainMainer from "./components/Products/MainMainer";
+const App = () => {
+  return (
+    <>
+    
+     <MainHeader/>
+     <MainMainer/>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/header/Cabeza.Html.jsx
@@ -0,0 +1,49 @@
+import logoSneakers from "@/assets/images/logo.svg";
+import AvatarImagenes from "@/assets/images/image-avatar.png";
+import MenuIcon from "@/components/icons/MenuIcon";
+import CartIcon from "@/components/icons/CartIcon";
+import CloseIcon from "@/components/icons/CloseIcon";
+import { useState } from "react";
+const MainHeader = () => {
+
+  const [navClass, setNavClass] = useState('hidden bg-white  font-bold md:static md:mr-auto md:flex md:flex-row md:gap-4 md:h-auto md:bg-transparent md:p-0');
+
+  const handleOpenMenu = () => {
+    setNavClass("absolute top-0 left-0 flex h-full w-4/5 flex-col gap-y-[21px] bg-white p-8 font-bold md:static md:mr-auto md:flex md:flex-row md:gap-4 md:h-auto md:bg-transparent md:p-0")
+  };
+  const handleCloseMenu = () => {
+    setNavClass("hidden bg-white  font-bold md:static md:mr-auto md:flex md:flex-row md:gap-4 md:h-auto md:bg-transparent md:p-0")
+  };
+
+  return (
+    <>
+      <header className=" container mx-auto  flex items-center gap-4 bg-gray-400 px-4 py-8">
+        <button className="md:hidden" onClick={handleOpenMenu}>
+          <MenuIcon />
+        </button>
+        <img
+          src={logoSneakers}
+          alt="logoSneakers"
+          className="mr-auto mb-1  md:mr-0"
+        />
+        <nav className={navClass}>
+          <button className=" mb-8 md:hidden" onClick={handleCloseMenu}>
+            <CloseIcon />
+          </button>
+          <a href="#">Colecciones</a>
+          <a href="#">Hombre</a>
+          <a href="#">Mujer</a>
+          <a href="#">Sobre</a>
+          <a href="#">Contacto</a>
+        </nav>
+        <div className="flex gap-4">
+          <button>
+            <CartIcon />
+          </button>
+          <img src={AvatarImagenes} alt="" className="w-10" />
+        </div>
+      </header>
+    </>
+  );
+};
+export default MainHeader;
