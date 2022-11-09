2d827b9e384a35d120906956fc2126aa01e09393
Frontend_boantek <fabricioreynacordes@gmail.com>
2022-11-08 23:32:33 -0300

    Trabajo

--- src/components/header/Cabeza.Html.jsx
+++ src/components/header/Cabeza.Html.jsx
@@ -4,20 +4,26 @@ import MenuIcon from "@/components/icons/MenuIcon";
 import CartIcon from "@/components/icons/CartIcon";
 import CloseIcon from "@/components/icons/CloseIcon";
 import { useState } from "react";
+import NavLinkHeader from "@/components/header/NavLinkHeader";
 const MainHeader = () => {
-
-  const [navClass, setNavClass] = useState('hidden bg-white  font-bold md:static md:mr-auto md:flex md:flex-row md:gap-4 md:h-auto md:bg-transparent md:p-0');
+  const [navClass, setNavClass] = useState(
+    "hidden bg-white  font-bold md:static md:mr-auto md:flex md:flex-row md:gap-4 md:h-auto md:bg-transparent md:p-0"
+  );
 
   const handleOpenMenu = () => {
-    setNavClass("absolute top-0 left-0 flex h-full w-4/5 flex-col gap-y-[21px] bg-white p-8 font-bold md:static md:mr-auto md:flex md:flex-row md:gap-4 md:h-auto md:bg-transparent md:p-0")
+    setNavClass(
+      "absolute top-0 left-0 flex h-full w-4/5 flex-col gap-y-[21px] bg-white p-8 font-bold md:static md:mr-auto md:flex md:flex-row md:gap-4 md:h-auto md:bg-transparent md:p-0"
+    );
   };
   const handleCloseMenu = () => {
-    setNavClass("hidden bg-white  font-bold md:static md:mr-auto md:flex md:flex-row md:gap-4 md:h-auto md:bg-transparent md:p-0")
+    setNavClass(
+      "hidden bg-white  font-bold md:static md:mr-auto md:flex md:flex-row md:gap-4 md:h-auto md:bg-transparent md:p-0"
+    );
   };
 
   return (
     <>
-      <header className=" container mx-auto  flex items-center gap-4 bg-gray-400 px-4 py-8">
+      <header className=" container mx-auto  flex items-center gap-4   ">
         <button className="md:hidden" onClick={handleOpenMenu}>
           <MenuIcon />
         </button>
@@ -30,11 +36,11 @@ const MainHeader = () => {
           <button className=" mb-8 md:hidden" onClick={handleCloseMenu}>
             <CloseIcon />
           </button>
-          <a href="#">Colecciones</a>
-          <a href="#">Hombre</a>
-          <a href="#">Mujer</a>
-          <a href="#">Sobre</a>
-          <a href="#">Contacto</a>
+          <NavLinkHeader text="Colecciones" />
+          <NavLinkHeader text="Hombre" />
+          <NavLinkHeader text="Mujer" />
+          <NavLinkHeader text="Sobre" />
+          <NavLinkHeader text="Contacto" />
         </nav>
         <div className="flex gap-4">
           <button>
@@ -43,6 +49,7 @@ const MainHeader = () => {
           <img src={AvatarImagenes} alt="" className="w-10" />
         </div>
       </header>
+      <span className="container mx-auto hidden h-[1px] w-auto bg-slate-200 md:block"></span>
     </>
   );
 };
